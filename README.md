9c364aa42364ae37e66a077a6f28c5b58eacf126
Neffc <calebjneff@gmail.com>
2020-12-31 10:26:14 -0600

    Refactoring, added debug flag for external troubleshooting.

--- README.md
+++ README.md
@@ -1,28 +1,28 @@
 NARG  
 (Neff's Alchemy Recipe Generator)  
   
-A tool I made to generate alchemy recipes for Noita. Contains robust search capability to find seeds with specific LC / AP formulas.
+A tool to generate alchemy recipes for Noita. Contains robust search capability to find seeds with specific LC / AP formulas.
   
 Usage: narg [options] SEED  
 Usage: narg -s [LC#1, LC#2, LC#3, AP#1, AP#2, AP#3]  
 Options:  
-    `-a`, `--array`         prints in comma-delimited format (seed#,x,x,x,y,y,y)  
-    `-s`, `--search`        search all seeds for a given recipe
-                            -> (can use * to sub any ingredient as a wildcard)  
     `-l`, `--list`          list all possible alchemy ingredients  
+    `-s`, `--search`        search all seeds for a given recipe  
+                            -> (can use * to sub any ingredient as a wildcard)  
+    `-d`, `--debug`         prints calculated values with seed; ignored when using search flag  
     `-h`, `--help`          print this help menu  
   
-Search has been added to narg:  
- - Each recipe will contain at most 1 "Solid" ingredient. Valid recipes contain either 3 "Liquids", or 2 "Liquids" & 1 "Solid". 6 ingredients total are needed to perform a search.  
- - Seeds are unable to generate with duplicates and/or elements that aren't contained in the "Liquids" or "Solids" categories.  
- - If searching for a recipe with an ingredient in the "Solids" category, both recipes must include a solid at the SAME POSITION (i.e. both LC#1 and AP#1 must be solids, or both LC#2 and AP#2 must be solids, but do not have to be the same solid). This is due to how the recipes are shuffled.  
+Info for recipe search:  
+ - Each recipe will contain at most 1 solid ingredient. Valid recipes contain either 3 liquids, or 2 liquids & 1 solid. 6 ingredients total are needed to perform a search.  
+ - Seeds are unable to generate recipes containing duplicate ingredients, or elements that aren't contained in the liquids or solids categories.  
+ - If searching for a recipe with an ingredient in the solids category, both recipes must include a solid at the SAME POSITION (i.e. both LC#1 and AP#1 must be solids, or both LC#2 and AP#2 must be solids, but do not have to be the same solid). This is due to how the recipes are shuffled.  
  - If you attempt to search something that doesn't meet these requirements, narg will still attempt to search for the recipe, but will not return any results. Typos or incorrect ingredient names will be defaulted to a wildcard during search (using the `--list` option will show the possible ingredient names for both categories).  
  - Since ingredient #2 for both recipes is the ingredient that is consumed, ingredients #1 & #3 are treated interchangeably for both recipes.  
  
   
   
-  Example: `narg -s oil water blood oil water alcohol`  
-Above example will search for a seed with a Lively Concoction recipe with oil, water, and blood, and an Alchemic Precursor recipe with oil, water, and alcohol.  
+  Example: `narg -s blood water oil water oil alcohol`  
+Above example will search for a seed with a Lively Concoction recipe with blood, water, and oil, and an Alchemic Precursor recipe with water, oil, and alcohol.  
   
 If you have any questions or encounter any issues, feel free to message me on Discord or ping me in the Noita discord server @Neff#6398.
 
--- src/main.rs
+++ src/main.rs
@@ -2,31 +2,13 @@ extern crate getopts;
 use getopts::Options;
 use std::time::Instant;
 use std::env;
-use alchemy::alchemy;
-use alchemy::lgm_random;
-use alchemy::materials;
+use alchemy::LIQUIDS;
+use alchemy::SOLIDS;
 use alchemy::init;
-use alchemy::init_shuffle;
+use alchemy::recipe;
 
 mod alchemy;
 
-const LIQUIDS: [&str; 30] = ["acid","alcohol","blood",
-    "blood_fungi","blood_worm","cement",
-    "lava","berserkium","pheromone",
-    "levitatium","hastium","invisiblium",
-    "concentrated_mana","acceleratium","ambrosia",
-    "teleportatium","unstable_polymorphine","unstable_teleportatium",
-    "worm_pheromone","flummoxium","mud",
-    "oil","poison","toxic_sludge",
-    "swamp","urine","water",
-    "water_ice","water_swamp","chaotic_polymorphine"];
-const SOLIDS: [&str; 18] = ["bone","brass","coal",
-    "copper","diamond","fungus",
-    "gold","grass","gunpowder",
-    "gunpowder_explosive","rotten_meat","sand",
-    "silver","slime","snow",
-    "soil","wax","honey"];
-const F32: f64 = i32::max_value() as f64;
 const SEEDMAX: i64 = 4294967295;
 
 fn print_about() {
@@ -43,26 +25,22 @@ fn main() {
     let args: Vec<String> = env::args().collect();
     let mut opts = Options::new();
     let mut v = 0;
-    opts.optflag("a", "array", "print in comma-delimited format (seed#,x,x,x,y,y,y)");
+    opts.optflag("l", "list", "list all possible alchemy ingredients");
     opts.optflag("s", "search", "search all seeds for a given recipe
                 \n-> (can use * to sub any ingredient as a wildcard)");
-    opts.optflag("l", "list", "list all possible alchemy ingredients");
-    opts.optflag("x", "silent", "print no output");
+    opts.optflag("d", "debug", "prints calculated values with seed; ignored when using search flag");
     opts.optflag("h", "help", "print this help menu");
-    let matches = match opts.parse(&args[1..]) {
+    let matches = match opts.parse(&args[0..]) {
         Ok(m) => { m }
         Err(f) => { panic!(f.to_string()) }
     };
-    let input = if !matches.free.is_empty() {
+    let input = if !matches.free.is_empty() && args.len() > 1 {
         matches.free.clone()
     } else {
         print_about();
         print_usage(opts);
         return;
     };
-    if matches.opt_present("a") {
-        v+=1;
-    }
     if matches.opt_present("l") {
         println!("\nLiquids:{:?}",LIQUIDS);
         println!("\nSolids:{:?}\n",SOLIDS);
@@ -72,12 +50,9 @@ fn main() {
         print_usage(opts);
         return;
     }
-    if matches.opt_present("x") {
-        v+=100;
-    }
-    if matches.opt_present("s") {
+    if matches.opt_present("s"){
         v+=10;
-        if input.len() != 6 {
+        if input.len() != 7 {
             println!("\n  Searching requires 6 ingredients. Check your ingredients, and try
                     \ragain. Use option '-l' or see the README.md for additional help.\n
                     \r    -s, --search  Searches all {} seeds for a given recipe.\n
@@ -89,123 +64,91 @@ fn main() {
                     ",SEEDMAX);
             return;
         }
-        let s_mats: [&str; 6] = [&input[0],&input[1],&input[2],&input[3],&input[4],&input[5]];
-        let seed = 1;
-        search(s_mats,seed,v);
+        let s_mats: [&str; 6] = [&input[1],&input[2],&input[3],&input[4],&input[5],&input[6]];
+        search(s_mats,v);
         return;
     }
-    let seed = input[0].parse::<i64>().unwrap();
+    let seed = input[1].parse::<i64>().unwrap();
     if seed > SEEDMAX {
         println!("Seeds only go up to {}, try again.",SEEDMAX);
         return;
     }
-    let (mats,prob) = alchemy(seed);
-    print_recipe(seed,mats,v,prob);
+    let (lc,lc_prob,iseed) = recipe(seed,init(seed));
+    let (ap,ap_prob,_x) = recipe(seed,iseed);
+    print_recipe(seed,lc,ap,v,[lc_prob, ap_prob]);
+    if matches.opt_present("d") {
+        print_debug(seed,lc[3],ap[3]);
+        return;
+    }
 }
 
-fn recipe_with_search(mut iseed: i64, shuf: [usize; 3], s_mats: [&str; 6]) -> ([&'static str; 6], [i64; 2]) {
-    let mut mats: [&str; 6] = [""; 6];
-    let mut i = 0;
-    let mut prob: [i64; 2] = [0,0];
-    for x in 0..2 {
-        iseed = lgm_random(iseed,1);
-        let (array,tmp) = materials(iseed,shuf);
-        iseed = tmp;
-        for n in 0..3 {
-            mats[i] = array[n];
-            i+=1;
-        }
-        if mats[1+3*x] == s_mats[1+3*x] || s_mats[1+3*x] == "*" {
-            iseed = lgm_random(iseed,1);
-            prob[x] = 10 - ((iseed as f64 / F32) as f64 * -91.0) as i64;
-        } else {
-            break;
-        }
-    };
-    return (mats,prob);
-}
-
-fn print_recipe(seed: i64, mats: [&str; 6], v: usize, prob: [i64; 2]) {
+fn print_recipe(seed: i64, lc: [&str; 4], ap: [&str; 4], v: usize, prob: [i64; 2]) {
     match v {
         0 => {
             println!("\nSeed: {}
             \rLively Concoction: {}, {}, {}
             \rAlchemic Precursor: {}, {}, {}
             \rLively Concoction Probability: {}%; Alchemic Precursor Probability: {}%\n"
-            ,seed,mats[0],mats[1],mats[2],mats[3],mats[4],mats[5],prob[0],prob[1]);
+            ,seed,lc[0],lc[1],lc[2],ap[0],ap[1],ap[2],prob[0],prob[1]);
             },
-        1 | 11 => println!("{},{},{},{},{},{},{}",seed,mats[0],mats[1],mats[2],mats[3],mats[4],mats[5]),
-        10 => println!("Seed: {}    LC: {},{},{}    AP: {},{},{}",seed,mats[0],mats[1],mats[2],mats[3],mats[4],mats[5]),
+        10 => println!("Seed: {}    LC: {},{},{}    AP: {},{},{}",seed,lc[0],lc[1],lc[2],ap[0],ap[1],ap[2]),
         _ => return,
     }
 }
 
-fn search(mut s_mats: [&str; 6], mut seed: i64, v: usize) {
+fn search(mut s_mats: [&str; 6], v: usize) {
     let now = Instant::now();
-    let mut i = SEEDMAX;
     let mut j = 0;
-    let mut x = 0;
-    let mut y = 0;
-    for n in 0..6 {
-        if LIQUIDS.contains(&s_mats[n]) {
-            x+=1;
-        } else if SOLIDS.contains(&s_mats[n]) {
-            y+=1;
-        } for n in 0..6 {
-            if !(LIQUIDS.contains(&s_mats[n])) && !(SOLIDS.contains(&s_mats[n])) && s_mats[n] != "*" {
-                println!("'{}' not found! Replacing with wildcard (*)!",s_mats[n]);
-                s_mats[n] = "*";
-            }
-        }
+    let mut seed = 1;
+  	for n in 0..6 {
+    	if !(LIQUIDS.contains(&s_mats[n])) && !(SOLIDS.contains(&s_mats[n])) && s_mats[n] != "*" {
+        	println!("'{}' not found! Replacing with wildcard (*)!",s_mats[n]);
+        	s_mats[n] = "*";
+    	}
     }
     println!("Searching for:{:?}",s_mats);
-    while i >= 0 {
-        let iseed = init(seed as f64);
-        if x == 6 {
-            let check = LIQUIDS[(lgm_random(iseed,2) as f64 / F32 * LIQUIDS.len() as f64) as usize];
-            if check == s_mats[0] || check == s_mats[1] || check == s_mats[2] {
-                let shuf = init_shuffle(seed);
-                let (mats,prob) = recipe_with_search(iseed,shuf,s_mats);
-                if is_valid(mats,s_mats) == true {
-                    print_recipe(seed,mats,v,prob);
-                    j+=1;
-                }
-            }
-        } else if (x+y) == 6 {
-            let tmp = lgm_random(iseed,2);
-            let check1 = LIQUIDS[(tmp as f64 / F32 * LIQUIDS.len() as f64) as usize];
-            let check2 = SOLIDS[(tmp as f64 / F32 * SOLIDS.len() as f64) as usize];
-            if s_mats.contains(&check1) || s_mats.contains(&check2) {
-                let shuf = init_shuffle(seed);
-                let (mats,prob) = recipe_with_search(iseed,shuf,s_mats);
-                if is_valid(mats,s_mats) == true {
-                    print_recipe(seed,mats,v,prob);
-                    j+=1;
-                }
-            }
-        } else {
-            let shuf = init_shuffle(seed);
-            let (mats,prob) = recipe_with_search(iseed,shuf,s_mats);
-            if is_valid(mats,s_mats) == true {
-                print_recipe(seed,mats,v,prob);
-                j+=1;
-            }
-        }
-        seed+=1;
-        i-=1;
-    }
-    println!("Time elapsed: {}.{} seconds!",now.elapsed().as_secs(), now.elapsed().subsec_millis());
+
+    while seed <= SEEDMAX {
+	    let (lc,lc_prob,iseed) = recipe(seed,init(seed));
+	    if is_valid(lc,[s_mats[0],s_mats[1],s_mats[2]]) == true {
+	    	let (ap,ap_prob,_x) = recipe(seed,iseed);
+	    	if is_valid(ap,[s_mats[3],s_mats[4],s_mats[5]]) == true {
+	    		print_recipe(seed,lc,ap,v,[lc_prob, ap_prob]);
+	    		j+=1;
+	    	}
+	    }
+	    seed+=1;
+	}
+	println!("Time elapsed: {}.{} seconds!",now.elapsed().as_secs(), now.elapsed().subsec_millis());
     println!("{} results found!",j);
 }
 
-fn is_valid(mats: [&str; 6], s_mats: [&str; 6]) -> bool{
-    if ((mats[1] == s_mats[1] || s_mats[1] == "*") && (mats[4] == s_mats[4] || s_mats[4] == "*")) &&
+fn is_valid(mats: [&str; 4], s_mats: [&str; 3]) -> bool{
+    if (mats[1] == s_mats[1] || s_mats[1] == "*") &&
        (mats[0] == s_mats[0] || mats[2] == s_mats[0] || s_mats[0] == "*") &&
-       (mats[0] == s_mats[2] || mats[2] == s_mats[2] || s_mats[2] == "*") &&
-       (mats[3] == s_mats[3] || mats[5] == s_mats[3] || s_mats[3] == "*") &&
-       (mats[3] == s_mats[5] || mats[5] == s_mats[5] || s_mats[5] == "*") {
+       (mats[0] == s_mats[2] || mats[2] == s_mats[2] || s_mats[2] == "*") {
         return true;
     } else {
         return false
     }
+}
+
+fn print_debug(seed: i64, lc: &str, ap: &str) {
+	let fmax: f64 = i32::max_value() as f64;
+	let mut iseed = init(seed);
+	for n in 0..14 {
+		println!("PRNG value #{}: {}", n,iseed);
+		iseed = alchemy::lgm_random(iseed,1);
+	}
+	let mut nseed = (seed as i64 >> 1) + 12534;
+	println!("Shuffle seed: {}",nseed);
+    nseed = alchemy::lgm_random(nseed,1);
+    let mut index: [usize; 3] = [0; 3];
+    for n in 0..3 {
+        nseed = alchemy::lgm_random(nseed,1);
+        index[n] = (nseed as f64 / fmax * ( (3 - n) as f64 + 1.0)) as usize;
+    }
+    println!("Shuffle index: {:?}",index);
+    println!("LC ingredient shuffled out: {}",lc);
+    println!("AP ingredient shuffled out: {}",ap);
 }
